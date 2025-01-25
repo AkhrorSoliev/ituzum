2053c59072fe70bf4892fe5c02bfc4b37d3ffddb
Akhror <akhsoliev@gmail.com>
2025-01-25 20:37:53 +0500

    feat: implement GlobalContext for state management; wrap App component with GlobalContextProvider to manage products and pricing

---
+++ src/context/globalContext.jsx
@@ -0,0 +1,38 @@
+import { useContext, useReducer } from "react";
+
+export const GlobalContext = createContext();
+
+const initialState = {
+  porducts: [],
+  totalPrice: 0,
+  totalQuantity: 0,
+};
+
+const changeState = (state, action) => {
+  switch (action.type) {
+    case "ADD_PRODUCT":
+      return {
+        ...state,
+        porducts: [...state.porducts, action.payload],
+      };
+    case "REMOVE_PRODUCT":
+      return {
+        ...state,
+        porducts: state.porducts.filter(
+          (product) => product.id !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+};
+
+export function GlobalContextProvider({ children }) {
+  const [state, dispatch] = useReducer(changeState, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+}
