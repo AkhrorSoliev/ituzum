cd9aa2b62355f15de3066aabcbe3c9ab33810a69
Akhror <akhsoliev@gmail.com>
2025-01-26 10:11:47 +0500

    feat: add react-hot-toast for notifications; update cart management to include total price and quantity calculations

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 import { GlobalContextProvider } from "./context/globalContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <GlobalContextProvider>
     <App />
+    <Toaster />
   </GlobalContextProvider>
 );
